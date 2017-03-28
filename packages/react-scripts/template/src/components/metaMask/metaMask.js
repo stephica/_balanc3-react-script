4b86b2a5a4e6ac586cf0dabbd7f4f941fd40ae01
John D. Storey <johndangerstorey@gmail.com>
2017-03-27 22:46:03 -0600

    updates dependencies

--- packages/react-scripts/template/src/components/metaMask/metaMask.js
+++ packages/react-scripts/template/src/components/metaMask/metaMask.js
@@ -1,14 +1,13 @@
-import { findDOMNode } from 'react-dom';
-import { compose, lifecycle, withState, withProps } from 'recompose';
+import { compose, lifecycle, withState } from 'recompose';
 
 const Ethereum = compose(
   withState('accounts', 'setAccounts', []),
   lifecycle({
     componentDidMount() {
-      if (web3) {
+      if (window.web3) {
         window.setTimeout(
           () => {
-            this.props.setAccounts(web3.eth.accounts);
+            this.props.setAccounts(window.web3.eth.accounts);
           },
           100
         );
