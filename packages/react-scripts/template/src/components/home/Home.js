2a751ea2bc83fb86b30e7430eec3b6fa5e8ca475
John D. Storey <johndangerstorey@gmail.com>
2017-05-01 00:28:37 -0600

    updates with some clarification on the state of the repo

--- packages/react-scripts/template/src/components/home/Home.js
+++ packages/react-scripts/template/src/components/home/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Buffer from '../buffer';
-import StyledLink from '../styled_link';
-import Button from '../button';
-import { Ethereum } from '../ethereumRedux';
-import TxSignButton from '../txSignButton';
-import styled from 'styled-components';
+import React from 'react'
+import Buffer from '../buffer'
+import StyledLink from '../styled_link'
+import Button from '../button'
+import { Ethereum } from '../ethereumRedux'
+import TxSignButton from '../txSignButton'
+import styled from 'styled-components'
 
 const Box = styled('div')`
   padding: 20px;
@@ -12,10 +12,10 @@ const Box = styled('div')`
   border: 3px solid;
   border-radius: 5px;
   word-wrap: break-word;
-`;
+`
 
-const GoodBox = styled(Box)` border-color: ${props => props.theme.primary} `;
-const BadBox = styled(Box)` border-color: ${props => props.theme.danger} `;
+const GoodBox = styled(Box)` border-color: ${props => props.theme.primary} `
+const BadBox = styled(Box)` border-color: ${props => props.theme.danger} `
 
 export default () => (
   <Buffer>
@@ -23,20 +23,20 @@ export default () => (
 
     <Ethereum>
       {({ accounts = [], eth }) => {
-        const account = accounts.length ? accounts[0] : null;
+        const account = accounts.length ? accounts[0] : null
         return (
           <div>
             {account &&
               <GoodBox>
-                Awesome! Ethereum is detected on your browser with accounts:
+                Awesome! Ethereum is detected on your browser with account:
                 {' '}
                 {account}
-                <p>
+                {/*<p>
                   Go ahead and press the button to sign an example transaction.
                 </p>
                 <div style={{ display: 'block' }}>
                   <TxSignButton />
-                </div>
+                </div>*/}
               </GoodBox>}
             {!account &&
               <BadBox>
@@ -46,9 +46,7 @@ export default () => (
                 {' '}
                 and using the
                 {' '}
-                <StyledLink
-                  to="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"
-                >
+                <StyledLink to="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">
                   {' '}chrome extension{' '}
                 </StyledLink>
                 {' '}
@@ -63,11 +61,11 @@ export default () => (
                 </div>
               </BadBox>}
           </div>
-        );
+        )
       }}
     </Ethereum>
     <p>
-      The box above box is an example of a "styled component" that detects an Ethereum account, most likely from Meta Mask. Next is a React Router link that will take you to a
+      The box above box is an example of a "styled component" that detects an Ethereum account, most likely from a local testrpc.js file or Meta Mask -- depending on my phase of development. Next is a React Router link that will take you to a
       {' '}
       <StyledLink to="/redux">redux example</StyledLink>
       {' '}
@@ -100,13 +98,18 @@ export default () => (
           styled-components
         </StyledLink>
       </li>
+      <li>
+        <StyledLink to="https://github.com/jeffbski/redux-logic">
+          redux-logic
+        </StyledLink>
+      </li>
     </ul>
     <p>
-      Oh yeah, one more thing.  This repo is a personal project that I make no promises about maintaining.  If you've somehow stumbled upon this and want to reach me, email me at
+      Oh yeah, one more thing: This repo is a personal project and is in development.  Some of it is half finished and I make no promises about maintaining.  If you've somehow stumbled upon this and want to reach me, email me at
       {' '}
       <StyledLink to="mailto:johndangerstorey@gmail.com">
         johndangerstorey@gmail.com
       </StyledLink>
     </p>
   </Buffer>
-);
+)
